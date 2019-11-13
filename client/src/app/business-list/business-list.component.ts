f4f5654d37126c648df4266acf356f095e31a5f0
valorad <valorad@outlook.com>
2019-11-12 21:10:21 -0500

    client: businessList: show search result on map

--- client/src/app/business-list/business-list.component.ts
+++ client/src/app/business-list/business-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-business-list',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessListComponent implements OnInit {
 
+  private _bList = [];
+
+  filteredBList = [];
+
+  @Input()
+  set bList(nextList: any[]) {
+    this._bList = nextList;
+  }
+  get bList() {
+    return this._bList;
+  }
+
+  @Output() nextBList = new EventEmitter<any[]>();
+
+  setNextBList = () => {
+    // TODO:
+    this.filteredBList = this._bList.slice(0, 1);
+    this.nextBList.emit(this.filteredBList)
+    return this.filteredBList;
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
--- client/src/app/index/index.component.ts
+++ client/src/app/index/index.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Business } from '../_interfaces/business.interface';
 
 import { BusinessService } from '../_services/business.service';
+import { LatLngLiteral } from '@agm/core';
 
 @Component({
   selector: 'app-index',
@@ -13,10 +14,13 @@ export class IndexComponent implements OnInit {
 
   currentLocation = {
     longitude: -73.578199,
-    latitude: 45.495514
+    latitude: 45.495514,
+    zoom: 8
   }
 
   businessList: any[] = [];
+
+  businessShowOnMap: any[] = [];
 
   constructor(
     private businessService: BusinessService
@@ -24,12 +28,28 @@ export class IndexComponent implements OnInit {
 
   ngOnInit() {
     this.getBusinessList();
-    
   }
 
   getBusinessList = async () => {
     this.businessList = await this.businessService.getListByLocation(this.currentLocation.longitude, this.currentLocation.latitude);
-    console.log(this.businessList);
   }
+
+  setNewMarkers = (newList: any[]) => {
+    this.businessShowOnMap = newList;
+    // move to first marker
+    if (newList.length > 0) {
+      let position = newList[0].location.coordinates;
+      this.setCurrentLocation(position[0], position[1], 16)
+    }
+  }
+
+  setCurrentLocation = (longitude: number, latitude: number, zoom=8) => {
+    this.currentLocation = {
+      ...this.currentLocation,
+      longitude,
+      latitude,
+      zoom: 16
+    }
+  };
 
 }
