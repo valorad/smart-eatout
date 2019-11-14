48b480a0418df61d1c2d9353cc4b1cd68996a589
valorad <valorad@outlook.com>
2019-11-14 13:11:48 -0500

    client: top component init

--- client/src/app/top/top.component.ts
+++ client/src/app/top/top.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Business } from '../_interfaces/business.interface';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
   styleUrls: ['./top.component.scss']
 })
-export class TopComponent implements OnInit {
+export class TopComponent {
+
+  private _bList = [];
+
+  bListThrottled = [];
+
+  businessTops: Business[] = [];
 
   modes = [
     {
@@ -20,13 +28,31 @@ export class TopComponent implements OnInit {
 
   currentMode = "allTime";
 
+  @Input()
+  set bList(nextList: Business[]) {
+    this._bList = nextList;
+    this.bListThrottled = this.throttleBusiness(20);
+    this.getAllTimeTops(10);
+  }
+  get bList() {
+    return this._bList;
+  }
+
   selectMode = (nextMode: string) => {
     this.currentMode = nextMode;
   };
 
-  constructor() { }
+  throttleBusiness = (num: number) => {
+    return this.bList.slice(0, num);
+  };
 
-  ngOnInit() {
-  }
+  getAllTimeTops = (top: number) => {
+    this.bListThrottled.sort( (businessA, businessB) => {
+      return businessB.stars - businessA.stars
+    } );
+    this.businessTops = this.bListThrottled.slice(0, top);
+  };
+
+  constructor() { }
 
 }
